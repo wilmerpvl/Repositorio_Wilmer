9371ef47e1830aeb3c4f507cbfc8cc2273abdb2f
wilmerpvl <140012141+wilmerpvl@users.noreply.github.com>
2025-06-19 22:45:03 -0500

    Segundo commit: Agrega función mostrarSemestre en main.js

--- main.js
+++ main.js
@@ -1,1 +1,5 @@
 console.log('Hola, soy Wilmer Pedro Villon Lavayen, estudiante de 9no semestre');
+"// Funci¢n de prueba para mostrar semestre" 
+"function mostrarSemestre() {" 
+"  console.log('Estoy en 9no semestre');" 
+"}" 
